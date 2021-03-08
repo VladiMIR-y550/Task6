bff9676d7ab954c3ac3779444a9617266c2f2c47
VladiMIR <vmirlock@gmail.com>
2021-03-08 18:34:55 +0200

    remade class QuickSearch - DRY & generic

--- src/QuickSearch.java
+++ src/QuickSearch.java
@@ -7,31 +7,22 @@ public class QuickSearch {
         return count;
     }
 
-    static <T> List<Object> getMatches(Collection<T> listNameOne, Collection<T> listNameTwo) {
+    static <T> List<T> getMatches(Collection<T> listNameOne, Collection<T> listNameTwo) {
 
-        Map<Object, String> first;
+        Map<T, String> first;
         List<T> second;
-        List<Object> tempList = new ArrayList<>();
+        List<T> tempList = new ArrayList<>();
 
         String emptyValue = "";
 
-        if (listNameOne.size() > listNameTwo.size()) {
-            second = new ArrayList<>(listNameTwo);
-            first = new HashMap<>();
-            for (Object one : listNameOne) {
-                count++;
-                first.put(one, emptyValue);
-            }
-        } else {
-            second = new ArrayList<>(listNameOne);
-            first = new HashMap<>();
-            for (Object two : listNameTwo) {
-                count++;
-                first.put(two, emptyValue);
-            }
+        second = new ArrayList<>(listNameTwo);
+        first = new HashMap<>();
+        for (T one : listNameOne) {
+            count++;
+            first.put(one, emptyValue);
         }
 
-        for (Object key : second) {
+        for (T key : second) {
             count++;
             if (first.get(key) != null) {
                 tempList.add(key);
